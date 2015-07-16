01c38a6f32eb2fbc43955bd49bf7b80e8e6a19e8
Awesomelytics <Awesomelytics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-16 09:53:09 +0000

    Adding link to register at Awesomelytics
    git-svn-id: https://plugins.svn.wordpress.org/awesomelytics/trunk@1199964 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- awesomelytics.php
+++ awesomelytics.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Awesomelytics
-Version: 0.1.2
+Version: 0.1.3
 Plugin URI: https://www.awesomelytics.com
 Author: Joshua McGee
 Author URI: http://joshuamcgee.com
@@ -13,4 +13,26 @@ function add_awesomelytics_scripts() {
   wp_enqueue_script( 'awesomelytics', 'https://www.awesomelytics.com/v1/a.js', array( 'awesomelytics-setup' ) );
 }
 add_action('wp_enqueue_scripts', 'add_awesomelytics_scripts');
+
+function awesomelytics_management_page() {
+  add_options_page( 'Awesomelytics', 'Awesomelytics', 'activate_plugins', 'awesomelytics', 'awesomelytics_settings' );
+}
+add_action('admin_menu', 'awesomelytics_management_page');
+
+// ### Maybe ###
+function add_awesomelytics_action_links ( $links ) {
+  $settings_link = '<a href="' . admin_url( 'options-general.php?page=awesomelytics' ) . '">' . 'Settings' . '</a>';
+  array_unshift( $links, $settings_link ); // before other links
+  return $links;
+}
+add_filter( 'plugin_action_links', 'add_awesomelytics_action_links' );
+
+function awesomelytics_settings() {
+  echo '<div class="wrap">';
+  echo '<h2>Awesomelytics</h2>';
+  echo '<p>';
+  echo '<a href="https://www.awesomelytics.com/join?domain=' . get_site_url() . '" target="_blank">Be sure to register your site at Awesomelytics!</a>';
+  echo '</p>';
+  echo '</div>';
+}
 ?>
